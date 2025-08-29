44ea6052732a72e31d3569294940fd469422f7cf
Muhammad Nasir Rasheed <142977146+nasirrasheed@users.noreply.github.com>
2025-08-29 10:46:11 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,26 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     host: "::",
     port: 8080,
-    // 👇 This is the key to fix React Router refresh 404s in dev
     historyApiFallback: true,
   },
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  // 👇 This ensures the production build also works with client-side routing
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
     },
   },
 });
